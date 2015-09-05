78061f4b49964142753ed6e5d49f34dab8ca3ffb
Mauricio Baeza <public@mauriciobaeza.net>
2015-09-05 13:47:27 -0500

    Tools methods refactory

--- source/pythonpath/easydev/tools.py
+++ source/pythonpath/easydev/tools.py
@@ -1,17 +1,16 @@
 # coding: utf-8
 
-import ctypes
-import subprocess
-import getpass
-import platform
-
 import sys
 import os
 import re
 import json
 import logging
-from string import Template
+import ctypes
+import subprocess
+import getpass
+import platform
 import csv
+from string import Template
 
 import uno
 import unohelper
@@ -19,11 +18,8 @@ from com.sun.star.beans import PropertyValue
 from com.sun.star.datatransfer import XTransferable, DataFlavor
 from org.universolibre.EasyDev import XTools
 
-    #~ from easydev.setting import DESKTOP, OS, WIN, WRITER, TOOLKIT, EXT_PDF, \
-        #~ NODE, NODE_CONFIG, LOG, NAME_EXT, BUTTONS_OK, BUTTONS_YES_NO, YES, \
-        #~ CLIPBOARD_FORMAT_TEXT
-
-from easydev.setting import LOG, NAME_EXT, OS, VERSION, DESKTOP
+from easydev.setting import LOG, NAME_EXT, OS, VERSION, DESKTOP, TOOLKIT,
+    BUTTONS_YES_NO, YES, NODE, NODE_CONFIG, CLIPBOARD_FORMAT_TEXT
 
 
 log = logging.getLogger(NAME_EXT)
@@ -38,10 +34,18 @@ class Tools(XTools):
         self.ctx = ctx
         self.sm = sm
 
+    def _create_instance(self, name, with_context=True):
+        if with_context:
+            instance = self.sm.createInstanceWithContext(name, self.ctx)
+        else:
+            instance = self.sm.createInstance(name)
+        return instance
+
     def getSizeScreen(self):
         if OS == WIN:
             user32 = ctypes.windll.user32
-            res = '{}x{}'.format(user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
+            res = '{}x{}'.format(
+                user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
         else:
             args = 'xrandr | grep "\*" | cut -d" " -f4'
             res = subprocess.check_output(args, shell=True).decode()
@@ -66,6 +70,190 @@ class Tools(XTools):
             platform.platform(),
         )
         return info
+
+    def question(self, title, message):
+        """ Create message box
+            type_msg: infobox, warningbox, errorbox, querybox, messbox
+        """
+        desktop = self._create_instance(DESKTOP)
+        toolkit = self._create_instance(TOOLKIT, False)
+        parent = toolkit.getDesktopWindow()
+        mb = toolkit.createMessageBox(
+            parent, 'querybox', BUTTONS_YES_NO, title, message)
+        return YES == mb.execute()
+
+    def render(self, template, data):
+        data = {r[0]: r[1] for r in data}
+        s = Template(template)
+        return s.safe_substitute(**data)
+
+    def format(self, template, data):
+        if isinstance(data, tuple):
+            if isinstance(data[0], tuple):
+                data = {r[0]: r[1] for r in data}
+                result = template.format(**data)
+            else:
+                result = template.format(*data)
+        else:
+            result = template.format(data)
+        return result
+
+    def path_to_os(self, path):
+        if path.startswith('file://'):
+            path = uno.fileUrlToSystemPath(path)
+        return path
+
+    def path_to_url(self, path):
+        if path.startswith('file://'):
+            return path
+        return uno.systemPathToFileUrl(path)
+
+    def getPath(self, name):
+        """
+            Return de path name in config
+            http://api.libreoffice.org/docs/idl/ref/interfacecom_1_1sun_1_1star_1_1util_1_1XPathSettings.html
+        """
+        if not name:
+            name = 'Work'
+        path = self._create_instance('com.sun.star.util.PathSettings')
+        return self.path_to_os(getattr(path, name))
+
+    def getPathInfo(self, path):
+        path = self.path_to_os(path)
+        path, filename = os.path.split(path)
+        name, extension = os.path.splitext(filename)
+        return (path, filename, name, extension)
+
+    def pathJoin(self, paths):
+        return os.path.normpath(os.path.join(*paths))
+
+    def getFolder(self, init_folder=''):
+        if init_folder:
+            init_folder = self.path_to_url(init_folder)
+        else:
+            init_folder = self.getPath('Work')
+        folder = self._create_instance('com.sun.star.ui.dialogs.FolderPicker')
+        folder.setDisplayDirectory(init_folder)
+        if folder.execute():
+            return folder.getDirectory()
+        else:
+            return ''
+
+    def getSelectedFiles(self, init_folder, multiple, filters):
+        if init_folder:
+            init_folder = self.path_to_url(init_folder)
+        else:
+            init_folder = self.getPath('Work')
+
+        folder = self._create_instance('com.sun.star.ui.dialogs.FilePicker')
+        folder.setDisplayDirectory(init_folder)
+        folder.setMultiSelectionMode(multiple)
+        if filters:
+            folder.setCurrentFilter(filters[0])
+            for i in range(0, len(filters), 2):
+                folder.appendFilter(filters[i], filters[i + 1])
+
+        if folder.execute():
+            files = folder.getSelectedFiles()
+            if multiple:
+                return files
+            else:
+                return files[0]
+        else:
+            return ""
+
+    def getFiles(self, path, ext):
+        paths = []
+        for folder, _, files in os.walk(path):
+            pattern = re.compile('\.{}'.format(ext), re.IGNORECASE)
+            paths += [os.path.join(folder, f) for f in files if pattern.search(f)]
+        return tuple(paths)
+
+    def fileOpen(self, path, mode='r'):
+        data = ''
+        if not mode:
+            mode = 'r'
+        path = path_to_os(path)
+        with open(path, mode) as f:
+            data = f.read()
+        return data
+
+    def execute(self, args, wait):
+        if wait:
+            res = subprocess.check_output(' '.join(args), shell=True).decode()
+            return res
+        else:
+            subprocess.Popen(args)
+        return
+
+    def getConfig(self, key):
+        name = 'com.sun.star.configuration.ConfigurationProvider'
+        cp = self._create_instance(name)
+        node = PropertyValue()
+        node.Name = 'nodepath'
+        node.Value = NODE
+        try:
+            ca = cp.createInstanceWithArguments(
+                'com.sun.star.configuration.ConfigurationAccess', (node,))
+            if ca and (ca.hasByName(NODE_CONFIG)):
+                data = ca.getPropertyValue(NODE_CONFIG)
+                if not data:
+                    return data
+                data = json.loads(data)
+                if key:
+                    value = data.get(key, '')
+                    if isinstance(value, list):
+                        return tuple(value)
+                    else:
+                        return value
+                else:
+                    return data
+            return
+        except Exception as e:
+            log.debug(e)
+            return
+
+    def setConfig(self, key, value):
+        name = 'com.sun.star.configuration.ConfigurationProvider'
+        cp = _create_instance(name)
+        node = PropertyValue()
+        node.Name = 'nodepath'
+        node.Value = NODE
+        try:
+            config_writer = cp.createInstanceWithArguments(
+                'com.sun.star.configuration.ConfigurationUpdateAccess', (node,))
+            data = self.getConfig('')
+            if not data:
+                data = {}
+            data[key] = value
+            new_values = json.dumps(data)
+            config_writer.setPropertyValue(NODE_CONFIG, new_values)
+            config_writer.commitChanges()
+            return True
+        except Exception as e:
+            log.debug(e)
+            return False
+
+    def getClipboard(self):
+        df = None
+        text = ''
+        sc = self._create_instance(
+            'com.sun.star.datatransfer.clipboard.SystemClipboard')
+        transferable = sc.getContents()
+        data = transferable.getTransferDataFlavors()
+        for df in data:
+            if df.MimeType == CLIPBOARD_FORMAT_TEXT:
+                break
+        if df:
+            text = transferable.getTransferData(df)
+        return text
+
+    def setClipboard(self, text):
+        ts = TextTransferable(text)
+        sc = self._create_instance(
+            'com.sun.star.datatransfer.clipboard.SystemClipboard')
+        sc.setContents(ts, None)
+        return
 
     def array(self, array, method, data):
         """
@@ -96,11 +284,6 @@ class Tools(XTools):
             return tuple(l)
         else:
             return res
-
-    def render(self, template, data):
-        data = {r[0]: r[1] for r in data}
-        s = Template(template)
-        return s.safe_substitute(**data)
 
 
 class TextTransferable(unohelper.Base, XTransferable):
@@ -134,14 +317,6 @@ class TextTransferable(unohelper.Base, XTransferable):
         return False
 
 
-def _create_instance(name=DESKTOP, with_context=True):
-    if with_context:
-        instance = SM.createInstanceWithContext(name, CTX)
-    else:
-        instance = SM.createInstance(name)
-    return instance
-
-
 def _make_properties(properties):
     prop = []
     l = len(properties)
@@ -151,10 +326,6 @@ def _make_properties(properties):
         pv.Value = properties[i + 1]
         prop.append(pv)
     return tuple(prop)
-
-
-def question(title, message):
-    return YES == msgbox(message, 'querybox', title, BUTTONS_YES_NO)
 
 
 def get_type_doc(doc):
@@ -229,13 +400,13 @@ def export_pdf(doc, path_save, options):
         doc = open_doc(doc, ('Hidden', True))
     if path_save:
         if os.path.isdir(path_save):
-            _, _, name, extension = get_path_info(path_os(doc.getURL()))
+            _, _, name, extension = get_path_info(path_to_os(doc.getURL()))
             path_save = path_to_url(
                 os.path.normpath('{}/{}.{}'.format(path_save, name, EXT_PDF)))
         else:
             path_save = path_to_url(path_save)
     else:
-        path_save = path_to_url(replace_ext(path_os(doc.getURL()), EXT_PDF))
+        path_save = path_to_url(replace_ext(path_to_os(doc.getURL()), EXT_PDF))
     type_doc = get_type_doc(doc)
     filters = {
         'calc': 'calc_pdf_Export',
@@ -252,38 +423,18 @@ def export_pdf(doc, path_save, options):
     doc.storeToURL(path_save, media_descriptor)
     if close:
         doc.dispose()
-    if os.path.exists(path_os(path_save)):
+    if os.path.exists(path_to_os(path_save)):
         return path_save
     return ''
 
 
-def path_to_url(path):
-    if path.startswith('file://'):
-        return path
-    return uno.systemPathToFileUrl(path)
 
 
-def path_os(path):
-    if path.startswith('file://'):
-        path = uno.fileUrlToSystemPath(path)
-    return path
 
 
-def get_path(name):
-    """
-        Return de path name in config
-        http://api.libreoffice.org/docs/idl/ref/interfacecom_1_1sun_1_1star_1_1util_1_1XPathSettings.html
-    """
-    if not name:
-        name = 'Work'
-    path = _create_instance('com.sun.star.util.PathSettings')
-    return getattr(path, name)
 
 
-def get_path_info(path):
-    path, filename = os.path.split(path)
-    name, extension = os.path.splitext(filename)
-    return (path, filename, name, extension)
+
 
 
 def replace_ext(path, ext):
@@ -291,162 +442,27 @@ def replace_ext(path, ext):
     return '{}/{}.{}'.format(path, name, ext)
 
 
-def path_join(paths):
-    return os.path.normpath(os.path.join(*paths))
 
 
-def get_folder(init_folder=''):
-    if init_folder:
-        init_folder = path_to_url(init_folder)
-    else:
-        init_folder = get_path('Work')
-    folder = _create_instance('com.sun.star.ui.dialogs.FolderPicker')
-    folder.setDisplayDirectory(init_folder)
-    if folder.execute():
-        return folder.getDirectory()
-    else:
-        return ''
 
 
-def get_selected_files(init_folder, multiple, filters):
-    if init_folder:
-        init_folder = path_to_url(init_folder)
-    else:
-        init_folder = get_path('Work')
-
-    folder = _create_instance('com.sun.star.ui.dialogs.FilePicker')
-    folder.setDisplayDirectory(init_folder)
-    folder.setMultiSelectionMode(multiple)
-    if filters:
-        folder.setCurrentFilter(filters[0])
-        for i in range(0, len(filters), 2):
-            folder.appendFilter(filters[i], filters[i + 1])
-
-    if folder.execute():
-        files = folder.getSelectedFiles()
-        if multiple:
-            return files
-        else:
-            return files[0]
-    else:
-        return ""
 
 
-def get_files(path, ext):
-    paths = []
-    for folder, _, files in os.walk(path):
-        pattern = re.compile('\.{}'.format(ext), re.IGNORECASE)
-        paths += [os.path.join(folder, f) for f in files if pattern.search(f)]
-    return tuple(paths)
 
 
-def execute(args, wait):
-    if wait:
-        res = subprocess.check_output(' '.join(args), shell=True).decode()
-        return res
-    else:
-        subprocess.Popen(args)
-    return
 
 
-def get_config(key):
-    name = 'com.sun.star.configuration.ConfigurationProvider'
-    cp = _create_instance(name)
-    node = PropertyValue()
-    node.Name = 'nodepath'
-    node.Value = NODE
-    try:
-        ca = cp.createInstanceWithArguments(
-            'com.sun.star.configuration.ConfigurationAccess', (node,))
-        if ca and (ca.hasByName(NODE_CONFIG)):
-            data = ca.getPropertyValue(NODE_CONFIG)
-            if not data:
-                return data
-            data = json.loads(data)
-            if key:
-                value = data.get(key, '')
-                if isinstance(value, list):
-                    return tuple(value)
-                else:
-                    return value
-            else:
-                return data
-        return
-    except Exception as e:
-        log.debug(e, exc_info=True)
-        return
 
 
-def set_config(key, value):
-    name = 'com.sun.star.configuration.ConfigurationProvider'
-    cp = _create_instance(name)
-    node = PropertyValue()
-    node.Name = 'nodepath'
-    node.Value = NODE
-    try:
-        config_writer = cp.createInstanceWithArguments(
-            'com.sun.star.configuration.ConfigurationUpdateAccess', (node,))
-        data = get_config('')
-        if not data:
-            data = {}
-        data[key] = value
-        new_values = json.dumps(data)
-        config_writer.setPropertyValue(NODE_CONFIG, new_values)
-        config_writer.commitChanges()
-        return True
-    except Exception as e:
-        log.debug(e, exc_info=True)
-        return False
 
 
-def file_open(path, mode='r'):
-    data = ''
-    if not mode:
-        mode = 'r'
-    path = path_os(path)
-    with open(path, mode) as f:
-        data = f.read()
-    return data
-
-
-def format(template, data):
-    if isinstance(data, tuple):
-        if isinstance(data[0], tuple):
-            data = {r[0]: r[1] for r in data}
-            result = template.format(**data)
-        else:
-            result = template.format(*data)
-    else:
-        result = template.format(data)
-    return result
-
-
-def get_text_from_clipboard():
-    df = None
-    text = ''
-    sc = _create_instance('com.sun.star.datatransfer.clipboard.SystemClipboard')
-    transferable = sc.getContents()
-    data = transferable.getTransferDataFlavors()
-    for df in data:
-        if df.MimeType == CLIPBOARD_FORMAT_TEXT:
-            break
-    if df:
-        text = transferable.getTransferData(df)
-    return text
-
-
-def set_text_to_clipboard(text):
-    ts = TextTransferable(text)
-    sc = _create_instance('com.sun.star.datatransfer.clipboard.SystemClipboard')
-    sc.setContents(ts, None)
-    return
 
 
 def export_csv(path, data, options=()):
     try:
         headers = ()
         config = {}
-        path = path_os(path)
+        path = path_to_os(path)
         if options:
             config = {r[0]: r[1] for r in options}
         write_headers = config.get('write_headers', False)
